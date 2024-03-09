de182d4d62fb267db3ddd6f8b74c3f9d20212a24
Diego Xavier <diegohenriquexavier2003@gmail.com>
2024-03-08 21:51:08 -0300

    upload

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,30 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import '../../assets/css/home.css'
 import Grafico from "../../assets/images/grafico.png"
 import Ufu from "../../assets/images/ufu.png"
 import '../../functions/valores.js'
 
 function Home(){
+
+    const [tempoAplicacao, setTempoAplicacao] = useState('2')
+    const [taxaAdministrativa, setTaxaAdministrativa] = useState('0')
+    const [valorSelic, setValorSelic] = useState('0.1125')
+    const [valorCdi, setValorCdi] = useState('0.1286')
+    const [taxaCustodia, setTaxaCustodia] = useState('0.002')
+
+    let Variaveis = {
+        tempoAplicacao,
+        taxaAdministrativa,
+        valorSelic,
+        valorCdi,
+        taxaCustodia
+    }
+    
+    const GuardarValores = () =>{
+        localStorage.setItem('Variaveis', JSON.stringify(Variaveis))
+    }
+    
+    GuardarValores()
 
     return(
         <main id='Home'>
