a88d2738e8a08965829c728e70349a24fdd526cc
Diego Xavier <diegohenriquexavier2003@gmail.com>
2024-03-09 15:27:05 -0300

    criação de calculos relacionados ao cdb1 e cdb3

--- src/functions/calculos.js
+++ src/functions/calculos.js
@@ -32,28 +32,96 @@ function Calcular(){
             return rendimentoLiquido * taxaIR
         }
 
-        const TesouroPrefixado = () =>{
+    const CdbNubank = ()=>{
+
+        const valorDoCdi = parseFloat(variaveis.valorCdi)
+        const tempoAplicacao = parseFloat(variaveis.tempoAplicacao);
+        const valorInvestido = parseFloat(investimentos.cdb1.split(';')[0])
+        const aportesMensais = parseFloat(investimentos.cdb1.split(';')[1])
+        const taxaCustodia = parseFloat(variaveis.taxaCustodia)*2
+        const taxaAdministrativa = parseFloat(variaveis.taxaAdministrativa) * tempoAplicacao
+        const taxaCDB = 1.07*valorDoCdi;
+        let ir = 0 
+        let TaxaCustodiaValor = 0
+        let rendimentoTotal = 0 
+        let valorInvestidoComOTempo = 0 
+        let totalTaxaAdministrativa = 0
+
+
+        let totalBruto = (valorInvestido+aportesMensais*tempoAplicacao*12)*(1+taxaCDB)**tempoAplicacao
+
+        for(let i =1;i<=tempoAplicacao;i++){
+            valorInvestidoComOTempo = (valorInvestido+aportesMensais*i*12)// ja ta acumulando
+            totalTaxaAdministrativa += valorInvestidoComOTempo*taxaAdministrativa
+            TaxaCustodiaValor += valorInvestidoComOTempo * taxaCustodia*2
+        }
+
+        rendimentoTotal = totalBruto - valorInvestidoComOTempo
+        
+        switch(tempoAplicacao){ 
+            case 1:
+                ir = calcularImpostoRenda(rendimentoTotal, 0.5)
+                break 
+            case 2:
+                ir = calcularImpostoRenda(rendimentoTotal, 1)
+                break
+            case 3:
+                ir = calcularImpostoRenda(rendimentoTotal, 2)
+                break 
+            case 4:
+                ir = calcularImpostoRenda(rendimentoTotal, 2)
+                break 
+            default:
+                ir = calcularImpostoRenda(rendimentoTotal, 5)
+                break
+        }
+    
+            const impostos = (ir + totalTaxaAdministrativa  + TaxaCustodiaValor)
+            const rendimentoLiquido = rendimentoTotal - impostos
+            const valorLiquido = valorInvestidoComOTempo + rendimentoLiquido
+    
+            const CDB1 = {
+                rendimentoTotal,
+                impostos,
+                ir,
+                valorInvestidoComOTempo,
+                taxaCustodia: TaxaCustodiaValor,
+                taxaAdministrativa: totalTaxaAdministrativa,
+                rendimentoLiquido,
+                valorLiquido,
+                valorBruto: valorInvestidoComOTempo + rendimentoTotal
+            }
+    
+            console.log(CDB1)
+            return CDB1
+    }
+
+    const TesouroPrefixado = () =>{
 
         const tempoAplicacao = parseFloat(variaveis.tempoAplicacao);
         const taxaAdministrativa = parseFloat(variaveis.taxaAdministrativa)
         const valorInvestido = parseFloat(investimentos.cdb2.split(';')[0])
         const aportesMensais = parseFloat(investimentos.cdb2.split(';')[1])
         const taxaCustodia = parseFloat(variaveis.taxaCustodia)
-    
         const tempoSemestral = parseFloat(tempoAplicacao)*2;
         const taxaCDB = 0.1064/2;
-        let semestre = 6
-        let bruto = 0
-        let ir = 0
+        let ir = 0 
         let TaxaCustodiaValor = 0
+        let rendimentoTotal = 0 
+        let valorInvestidoComOTempo = 0 
+        let totalDoPeriodo = 0 
+        let rendimentoDoPeriodo = 0 
+        let totalTaxaAdministrativa = 0
+        
     
         for(let i=1; i<=tempoSemestral; i++){
-            let totalDoPeriodo = (valorInvestido + aportesMensais * semestre * i)*(1+taxaCDB)
-            let valorInvestidoComOTempo = (valorInvestido + aportesMensais * semestre * i)
-            bruto += totalDoPeriodo
+
+            totalDoPeriodo = (valorInvestido + aportesMensais * 6 * i)*(1+taxaCDB)
+            valorInvestidoComOTempo = (valorInvestido + aportesMensais * 6 * i)
             TaxaCustodiaValor += (valorInvestidoComOTempo * taxaCustodia)
-            let rendimentoDoPeriodo = (totalDoPeriodo - valorInvestidoComOTempo)
-    
+            rendimentoDoPeriodo = (totalDoPeriodo - valorInvestidoComOTempo)
+            rendimentoTotal += rendimentoDoPeriodo
+            
             switch(i){ 
                 case 1:
                     ir += calcularImpostoRenda(rendimentoDoPeriodo, 0.5)
@@ -71,51 +139,109 @@ function Calcular(){
                     ir += calcularImpostoRenda(rendimentoDoPeriodo, 5)
                     break
             }
+
+            //totalTaxaAdministrativa
+            if(i%2 ===0){
+                totalTaxaAdministrativa += valorInvestidoComOTempo*taxaAdministrativa
+            }
+            
         }
     
-        const investido = valorInvestido + aportesMensais * semestre * tempoSemestral
-        const rendimentoBruto = bruto - investido
-        const impostos = (ir + rendimentoBruto * taxaAdministrativa  + TaxaCustodiaValor)
-        const rendimentoLiquido = rendimentoBruto - impostos
-        const valorLiquido = investido + rendimentoLiquido
-    
+        const impostos = (ir + totalTaxaAdministrativa  + TaxaCustodiaValor)
+        const rendimentoLiquido = rendimentoTotal - impostos
+        const valorLiquido = valorInvestidoComOTempo + rendimentoLiquido
+
         const CDB2 = {
-            rendimentoBruto,
+            rendimentoTotal,
             impostos,
             ir,
-            investido,
+            valorInvestidoComOTempo,
             taxaCustodia: TaxaCustodiaValor,
-            taxaAdministrativa: rendimentoBruto * taxaAdministrativa,
+            taxaAdministrativa: totalTaxaAdministrativa,
             rendimentoLiquido,
             valorLiquido,
-            valorBruto: bruto
+            valorBruto: valorInvestidoComOTempo + rendimentoTotal
         }
-
-        console.log(CDB2)
-    
         return CDB2
     }
-    
-    // const cdbNubank = ()=>{
+
+    const TesouroSelic = () =>{
+        const tempoAplicacao = parseFloat(variaveis.tempoAplicacao)*12;
+        const taxaSelic = parseFloat(variaveis.valorSelic)
+        const taxaAdministrativa = parseFloat(variaveis.taxaAdministrativa)
+        const valorInvestido = parseFloat(investimentos.cdb3.split(';')[0])
+        const aportesMensais = parseFloat(investimentos.cdb3.split(';')[1])
+        const taxaCustodia = parseFloat(variaveis.taxaCustodia)
+        const taxaCDB = taxaSelic/12 + (0.15/12)/100 //0.0095
+
+        let ir = 0 
+        let TaxaCustodiaValor = 0
+        let rendimentoTotal = 0 
+        let valorInvestidoComOTempo = 0 
+        let totalDoPeriodo = 0 
+        let rendimentoDoPeriodo = 0 
+        let totalTaxaAdministrativa = 0
+
+        for(let i =1; i<=tempoAplicacao; i++){
+            valorInvestidoComOTempo = (valorInvestido + aportesMensais * i)
+            totalDoPeriodo = (valorInvestido + aportesMensais * i) * (1 + taxaCDB)
+            rendimentoDoPeriodo =  totalDoPeriodo - valorInvestidoComOTempo
+            rendimentoTotal += rendimentoDoPeriodo
+            
+            if(i<=6){ 
+                ir += calcularImpostoRenda(rendimentoDoPeriodo, 0.5)
+            }
+            else if( i<=12){
+                ir += calcularImpostoRenda(rendimentoDoPeriodo, 1)
+            }
+            else if (i<=24){
+                ir += calcularImpostoRenda(rendimentoDoPeriodo, 2)
+            } 
+            else {
+                ir += calcularImpostoRenda(rendimentoDoPeriodo, 5)
+            }
+            
+            
+            
+            if(i%12===0){
+                
+                totalTaxaAdministrativa += taxaAdministrativa * valorInvestidoComOTempo
+
+                console.log(totalTaxaAdministrativa)
+                if(valorInvestidoComOTempo >= 10000){
+                    TaxaCustodiaValor += taxaCustodia * valorInvestidoComOTempo
+                } else {
+                    TaxaCustodiaValor = 0
+                }
+            }
+
+        }
         
-    //     const valorDoCdi = parseFloat(variaveis.valorCdi)
-    
-    //     let valorInvestido = investimentos.cdb1.split(';')
-    //     let aportes = parseFloat(valorInvestido[1])
-    //     valorInvestido = parseFloat(valorInvestido[0])
-        
-    //     const juros = (valorInvestido*(1.07*valorDoCdi))**2
-    
-    //     const imposto = juros*ir
-    
-    //     const montante = juros + valorInvestido - imposto
-    //     return juros
-    // }
-    // console.log("cdb nubank " + cdbNubank())
+        const impostos = (ir + totalTaxaAdministrativa  + TaxaCustodiaValor)
+        const rendimentoLiquido = rendimentoTotal - impostos
+        const valorLiquido = valorInvestidoComOTempo + rendimentoLiquido
+
+        const CDB3 = {
+            rendimentoTotal,
+            impostos,
+            ir,
+            valorInvestidoComOTempo,
+            taxaCustodia: TaxaCustodiaValor,
+            taxaAdministrativa: totalTaxaAdministrativa,
+            rendimentoLiquido,
+            valorLiquido,
+            valorBruto: valorInvestidoComOTempo + rendimentoTotal
+        }
+
+        console.log(CDB3)
+        return CDB3
+    }
     
     
     useEffect(()=>{
+        resultados.cdb1 = CdbNubank()
         resultados.cdb2 = TesouroPrefixado()
+        resultados.cdb3 = TesouroSelic()
         localStorage.setItem("Resultados", JSON.stringify(resultados))
         window.location.href = `https://simuladormatematicafinanceira.onrender.com/Resultados/${userName}/${userSurname}`
     })
